66a70143e4dd2ab866218af0be72dc450c35657f
kaushiksuman122 <kaushik.suman@sovratec.com>
2026-03-14 20:46:34 +0530

    Initial code for the Website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚗 HLO Drive</h1>
+        <p>Driver Aa Raha Hai</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
